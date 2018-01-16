bcea43965e8bdbdcbaacabe615ca3351495f5c96
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-01-16 11:57:47 -0700

    Build basic listen

--- server.js
+++ server.js
@@ -1,3 +1,9 @@
 const http = require('http');
 const url = require('url');
 const server = http.createServer();
+
+server.listen(3000, () => {
+  response.writeHead(200, { 'Content-Type': 'text/plain' });
+  response.write('Hello Wolrd\n');
+  response.end();
+});
