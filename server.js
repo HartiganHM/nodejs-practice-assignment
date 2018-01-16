ffdfe08b9dc41db0156c6e0dbe5f4c88a1a53d82
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-01-16 12:12:48 -0700

    Build messages

--- server.js
+++ server.js
@@ -2,6 +2,13 @@ const http = require('http');
 const url = require('url');
 const server = http.createServer();
 
+let messages = [
+  { 'id': 1, 'user': 'Hugh Hartigan', 'message': 'Beep-boop-beep-potato' },
+  { 'id': 2, 'user': 'Juliana Fargnoli', 'message': 'Cheese and wine and cheese and wine and...' },
+  { 'id': 3, 'user': 'Torin \'Bear\' Hartigan', 'message': 'Ball, ball, sleep, eat, ball, play, sleep, ball, sleep' },
+  { 'id': 4, 'user': 'Juniper \'Juni\' Fargnoli', 'message': 'Squirrel? AHHHHHH!! RAAA-BRRAAAA!!!' }
+]
+
 server.listen(3000, () => {
   console.log('The HTTP server is listening at Port 3000.');
 })
