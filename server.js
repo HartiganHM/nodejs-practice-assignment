80eef53b4a59e26cca53c904b7f33468f64bc7b0
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-01-16 12:15:33 -0700

    Build GET and POST for server

--- server.js
+++ server.js
@@ -14,7 +14,17 @@ server.listen(3000, () => {
 })
 
 server.on('request', (request, response) => {
-  response.writeHead(200, { 'Content-Type': 'text/plain' });
-  response.write('Hello World\n');
-  response.end();
+  if(request.method === 'GET') {
+    getAllMessages(response);
+  } else if (request.method === 'POST') {
+    let newMessage = { 'id': new Date() };
+
+    request.on('data', (data) => {
+      newMessage = Object.assign(newMessage, JSON.parse(data));
+    });
+
+    request.on('end' () => {
+      addMessage(newMessage, response);
+    });
+  }
 });
